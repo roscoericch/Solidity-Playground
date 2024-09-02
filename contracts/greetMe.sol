538648ed8e0cee27ee945c6d86c18630576f3d4f
roscoericch <sikirurazak1@gmail.com>
2024-09-02 16:37:54 +0100

    update greet contract

--- contracts/greetMe.sol
+++ contracts/greetMe.sol
@@ -4,9 +4,12 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract Greet {
     string public myName;
-
+    struct Visitor {string name;int visits;}
+    Visitor[] visitors;
     function greet(string memory name) public returns (string memory){
         myName = name;
+        if(visitors[name])
+        visitors.push(Visitor(name,1));
         return string(abi.encodePacked("Hello ,",name));
     }
 }
