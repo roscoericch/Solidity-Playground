e98391aa739ae1467c5436551edc5d4636d51d22
roscoericch <sikirurazak1@gmail.com>
2024-09-22 00:09:56 +0100

    update contract interactions

---
+++ contracts/storageFactory.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "contracts/1_Storage.sol";
+
+contract StorageFactory {
+    Storage[] public storageArray;
+    function createSimpleStorageContract() public {
+        Storage newStorage = new Storage();
+        storageArray.push(newStorage);
+    }
+
+    function storeNumber(uint256 _storageIndex, uint256 number) public {
+        Storage targetStorage = storageArray[_storageIndex];
+        targetStorage.store(number);
+        storageArray[_storageIndex] = targetStorage;
+    }
+
+    function getStoredValue(uint256 _storageIndex) public view returns (uint256){
+        Storage targetStorage = storageArray[_storageIndex];
+        return targetStorage.retrieve();
+    }
+}
